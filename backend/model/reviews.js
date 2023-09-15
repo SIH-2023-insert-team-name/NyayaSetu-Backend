8b5ce11f3ef38d11e30b331347e62760b2075243
palak <palakhandelwalgp@gmail.com>
2023-09-15 22:24:31 +0530

    added schemas

---
+++ backend/model/reviews.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema(
+  {
+    lawyer_id: {
+      type: String,
+      require: true,
+      
+    },
+    text: {
+      type: String,
+      required: true,
+      
+    },
+    rating: {
+      type: Number,
+      require: true,
+      min: 1,
+      max:5
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Review = mongoose.model("Review", reviewSchema);
+export default Review
