b5f82b3966849b8a422d01e2eb07ef24def2c1ac
Keshab Kataruka <keshab0121@gmail.com>
2023-09-16 01:21:28 +0530

    fix

--- backend/middleware/auth_user.js
+++ backend/middleware/auth_user.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const checkAuth = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.jwt_key);
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
     req.userData = decoded;
     next();
   } catch (error) {
